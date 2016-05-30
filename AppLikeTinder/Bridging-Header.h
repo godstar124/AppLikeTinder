1fddea9dbf435c511b135635a4649f7fa079aefa
master <business.star222@gmail.com>
2016-05-30 12:01:55 +0800

    no message

---
+++ AppLikeTinder/Bridging-Header.h
@@ -0,0 +1,15 @@
+//
+//  Bridging-Header.h
+//  AppLikeTinder
+//
+//  Created by Marko Budal on 31/08/15.
+//  Copyright (c) 2015 Marko Budal. All rights reserved.
+//
+
+#import <Parse/Parse.h>
+//#import <ParseUI/ParseUI.h>
+#import <Bolts/BFTask.h>
+#import <ParseFacebookUtils/PFFacebookUtils.h>
+#import <FacebookSDK/FacebookSDK.h>
+#import "JSQMessagesViewController/JSQMessages.h"
+#import <FireBase/Firebase.h>
